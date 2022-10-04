ef6705143fd3c8f1f7995cb92acd1fdb266f5fcb
nayyyhaa <neha@commutatus.com>
2022-10-04 14:51:10 +0530

    refactor: remove commas from json

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
